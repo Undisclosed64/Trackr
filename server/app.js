cfcfd545b4031be34a939468f9f01ab3049e65fe
Undisclosed <undisclosed674@gmail.com>
2022-10-01 12:29:39 -0700

    change routes use

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRouter = require("./routes/user");
-
+const projectRouter =  require("./routes/project");
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -15,7 +15,9 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/server/users", userRouter);
+app.use("/server", userRouter);
+app.use("/server", projectRouter);
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
