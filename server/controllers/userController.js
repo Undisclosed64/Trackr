7fcb0e3e93b641b32f933f50b4ed6eef3dab7891
Undisclosed <undisclosed674@gmail.com>
2022-10-01 12:25:57 -0700

    handle authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const { validationResult, check } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.createUser = [
   //validate input fields
@@ -10,7 +11,8 @@ exports.createUser = [
     .isLength({ min: 5 })
     .withMessage("Password must be at least 5 characters long"),
 
-  (req, res) => {
+  async (req, res) => {
+    //check for errors in req data
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
@@ -25,12 +27,8 @@ exports.createUser = [
       });
 
       //secure the password
-      bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(user.password, salt, function (err, hash) {
-          if (err) res.json(err);
-          user.password = hash;
-        });
-      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
 
       //save the user
       user.save((err, created_user) => {
@@ -40,3 +38,55 @@ exports.createUser = [
     }
   },
 ];
+
+exports.logUser = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  //if email is registered or not
+  if (user == null) res.status(404).json("Email is not registered!");
+  //if password is correct or not
+  if (await bcrypt.compare(req.body.password, user.password)) {
+    //generate an access token
+    const accessToken = jwt.sign(
+      { email: user.email, role: user.role },
+      process.env.key
+    );
+    //return user info and token
+    res.json({
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      role: user.role,
+      accessToken,
+    });
+  } else {
+    res.status(404).json("Password is incorrect!");
+  }
+};
+
+const verify = (req, res, next) => {
+  //get auth header
+  const authHeader = req.headers.authorization;
+
+  //split auth header
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    //verify token
+    jwt.verify(token, process.env.key, (err, user) => {
+      if (err) res.status(403).json("Token is invalid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json('You are not authorized to this route!')
+  }
+};
+
+
+
+// exports.deleteAll = function (req, res) {
+//   User.remove({}, function(err,success){
+//     if(err)res.json(err);
+//     res.json(success)
+//   })
+// };
