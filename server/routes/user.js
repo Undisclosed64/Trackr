dce250c5f9c5c6454fc89e6cabbf3f7a4c24f0f8
Undisclosed <undisclosed674@gmail.com>
2022-10-01 12:27:29 -0700

    add login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/", userController.createUser);
+router.post("/users", userController.createUser);
+
+router.post("/log-in", userController.logUser);
+
+// router.delete("/delete", userController.deleteAll);
 
 module.exports = router;
