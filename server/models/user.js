193946b4ff6ed0d18be404f45d275e52c5752412
Undisclosed <undisclosed674@gmail.com>
2022-10-08 11:20:45 -0700

    add lastname

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String },
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String },
