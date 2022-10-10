f89b77dc781132b778be05d15ad600fc0093c020
Undisclosed <undisclosed674@gmail.com>
2022-10-10 00:31:46 -0700

    import verifytoken

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const verifyToken = require("../verifyToken");
 
 router.post("/users", userController.createUser);
 
 router.post("/log-in", userController.logUser);
+
+router.get("/random", verifyToken, userController.secretRoute);
 
 // router.delete("/delete", userController.deleteAll);
 
