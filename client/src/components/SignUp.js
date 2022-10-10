483f7b094529916cf5e7714bfb7cd8b1b00a6c47
Undisclosed <undisclosed674@gmail.com>
2022-10-10 07:19:46 -0700

    add classname for error div

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -44,73 +44,81 @@ const SignUp = () => {
     }
   };
   return (
-    <Form className="signUpForm" onSubmit={handleSubmit}>
+    <div>
       <h1 className="signUpHeader">Sign Up</h1>
       {errors.length !== 0
         ? errors[0].map((err) => {
-            return <div key={err.param}>{err.msg}</div>;
+            return (
+              <div key={err.param} className="error">
+                {err.msg}
+              </div>
+            );
           })
         : ""}
-      <Form.Group className="mb-3">
-        <Row>
-          <Col>
-            <Form.Control
-              required
-              type="text"
-              placeholder="First name"
-              onChange={(e) =>
-                setFormData({ ...formData, firstName: e.target.value })
-              }
-              name="firstName"
-            />
-          </Col>
-          <Col>
-            <Form.Control
-              type="text"
-              placeholder="Last name"
-              onChange={(e) =>
-                setFormData({ ...formData, lastName: e.target.value })
-              }
-              name="lastName"
-            />
-          </Col>
-        </Row>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control
-          required
-          type="email"
-          placeholder="Enter email"
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          name="email"
-        />
-      </Form.Group>
+      <Form className="signUpForm" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Row>
+            <Col>
+              <Form.Control
+                required
+                type="text"
+                placeholder="First name"
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
+                name="firstName"
+              />
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Last name"
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
+                name="lastName"
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            name="email"
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control
-          required
-          type="password"
-          placeholder="Password"
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-          name="password"
-        />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            required
+            type="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            name="password"
+          />
+        </Form.Group>
 
-      <Form.Select
-        aria-label="Default select example"
-        onChange={(e) => setFormData({ ...formData, role: e.target.value })}
-      >
-        <option name="role">Select role</option>
-        <option value="developer">Developer</option>
-        <option value="project manager">Project manager</option>
-        <option value="admin">Admin</option>
-      </Form.Select>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Select
+          aria-label="Default select example"
+          onChange={(e) => setFormData({ ...formData, role: e.target.value })}
+        >
+          <option name="role">Select role</option>
+          <option value="developer">Developer</option>
+          <option value="project manager">Project manager</option>
+          <option value="admin">Admin</option>
+        </Form.Select>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 export default SignUp;
