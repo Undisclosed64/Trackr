4bffcb95ef3481dff5bd91e564a82bcad9263c24
Undisclosed <undisclosed674@gmail.com>
2022-10-14 06:30:16 -0700

    save accesstoken

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   const baseURL = "http://localhost:5000/server";
@@ -11,14 +13,20 @@ const LogIn = () => {
     password: "",
   });
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (data.email === "" || data.password === "") {
+      setError("Input field can not be empty!");
+      return;
+    }
     try {
       const res = await axios.post(`${baseURL}/log-in`, data);
       console.log(res.data);
       const token = res.data.accessToken;
       localStorage.setItem("token", token);
+      navigate("/dashboard");
     } catch (err) {
       if (err.response) {
         setError(err.response.data.msg);
@@ -54,6 +62,10 @@ const LogIn = () => {
           Submit
         </Button>
       </Form>
+      <div className="signUpLinkInLogin">
+        <div>New to X? </div>
+        <Link to="/sign-up">Sign Up</Link>
+      </div>
     </div>
   );
 };
