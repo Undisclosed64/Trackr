8fe835479c20a80b14c392af6eb073e4f595eccb
Undisclosed <undisclosed674@gmail.com>
2022-10-14 06:35:24 -0700

    add sidebar component

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div id="sidebar">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/create-project">Create Project</Link>
+          </li>
+          <li>
+            <Link to="/create-ticket">Add ticket</Link>
+          </li>
+          <li>
+            <Link to="/discuss">Discuss</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Sidebar;
