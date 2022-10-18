e10887a7352fb0ccb205ae44dc5db0996e93f036
Undisclosed <undisclosed674@gmail.com>
2022-10-18 12:04:33 -0700

    add project deleted

---
+++ client/src/components/ProjectDeleted.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+function ProjectDeleted() {
+  return (
+    <div className="projectDeletedPage">
+      <span>ERROR</span>
+      <h1>410</h1>
+      <p>The project has been deleted.</p>
+    </div>
+  );
+}
+
+export default ProjectDeleted;
