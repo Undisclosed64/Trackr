27c257d18a1c7a7925136b36535dff5e07108396
Undisclosed <undisclosed674@gmail.com>
2022-10-18 11:58:39 -0700

    get user

--- client/src/App.js
+++ client/src/App.js
@@ -6,32 +6,61 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogIn from "./components/LogIn";
 import Home from "./components/Home";
 import ProjectDetails from "./components/ProjectDetails";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LogIn />,
-  },
-  {
-    path: "sign-up",
-    element: <SignUp />,
-  },
-
-  {
-    path: "dashboard",
-    element: <Home />,
-  },
-  {
-    path: "create-project",
-    element: <CreateProject />,
-  },
-  {
-    path: "projects/:projectId",
-    element: <ProjectDetails />,
-  },
-]);
+import Ticket from "./components/Ticket";
+import GetBugs from "./components/GetBugs";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const baseURL = "http://localhost:5000/server";
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/getUser`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setUser(response.data.user);
+      });
+  }, []);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LogIn />,
+    },
+    {
+      path: "sign-up",
+      element: <SignUp />,
+    },
+
+    {
+      path: "dashboard",
+      element: <Home />,
+    },
+    {
+      path: "create-project",
+      element: <CreateProject />,
+    },
+    {
+      path: "projects/:projectId",
+      element: <ProjectDetails />,
+    },
+    {
+      path: "projects/:projectId/bugs",
+      element: <GetBugs user={user} />,
+    },
+    {
+      path: "tickets",
+      element: <Ticket />,
+    },
+  ]);
+
+  if (!user) return <div>loading...</div>;
+
   return (
     <div className="App">
       <RouterProvider router={router} />
