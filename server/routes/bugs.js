9da24c84de3019e26915690dd256391840243195
Undisclosed <undisclosed674@gmail.com>
2022-10-21 11:36:29 -0700

    add new route

--- server/routes/bugs.js
+++ server/routes/bugs.js
@@ -4,12 +4,12 @@ const bugController = require("../controllers/bugController");
 
 router.post("/bugs", bugController.CreateBugs);
 
-// router.get("/bugs", bugController.getAllBugs);
-
 router.get("/bugs", bugController.findBugsByProjects);
 
 router.get("/bugs/:id", bugController.getBug);
 
 router.put("/bugs/:id", bugController.updateBug);
+
+// router.delete("/bugs", bugController.deleteBugs);
 
 module.exports = router;
