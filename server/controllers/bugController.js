fa8a03dc867b0320c5604b6186a08ea7ab2c10d2
Undisclosed <undisclosed674@gmail.com>
2022-11-24 10:36:40 -0800

    change handler

--- server/controllers/bugController.js
+++ server/controllers/bugController.js
@@ -3,6 +3,7 @@ const { validationResult, check } = require("express-validator");
 const { ObjectId } = require("mongodb"); // or ObjectID
 const bugs = require("../models/bugs");
 const mongoose = require("mongoose");
+const async = require("async");
 
 //bug create handler
 exports.CreateBugs = [
@@ -36,6 +37,10 @@ exports.CreateBugs = [
         const username = email.substring(0, email.lastIndexOf("@"));
         bug.createdBy = { username, email };
       }
+      bug.trackActivities.push({
+        date: Date.now(),
+        updatedField: `${req.user.email} has added ticket ${bug.title}`,
+      });
       bug.save((err, createdBug) => {
         if (err) res.status(400).json(err);
         res.status(200).json(createdBug);
@@ -122,45 +127,46 @@ exports.findBugsByProjects = (req, res) => {
 // };
 
 //update a bug
-exports.updateBug = [
-  check("title").notEmpty().withMessage("Title can not be empty!"),
-  check("dueDate")
-    .isISO8601()
-    .toDate()
-    .withMessage("Due date does not have a valid format!"),
+exports.updateBug = async function (req, res, next) {
+  async.parallel(
+    {
+      bug: function (callback) {
+        Bug.findById(req.params.id).exec(callback);
+      },
+      update: function (callback) {
+        let updateField = {};
+        updateField[req.body.fieldName] = req.body.value;
+        console.log(updateField);
 
-  async (req, res) => {
-    //if errors return
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
-    else {
-      //create bug object
-      const bugToUpdate = new Bug({
-        project: req.body.project,
-        title: req.body.title,
-        description: req.body.description,
-        assignedDev: req.body.assignedDev,
-        bugType: req.body.bugType,
-        flag: req.body.flag,
-        severity: req.body.severity,
-        status: req.body.status,
-        dueDate: req.body.dueDate,
-        _id: req.params.id,
-      });
-      //find bug by id and update it
-      try {
-        Bug.findByIdAndUpdate(req.params.id, bugToUpdate, {
-          new: true,
-        }).exec((err, updatedBug) => {
-          if (err) res.status(400).json(err);
-          res.status(200).json(updatedBug);
-        });
-      } catch (err) {
-        res.status(500).json(err);
+        //find the bug and update
+        Bug.updateOne(
+          { _id: req.params.id },
+          {
+            $set: updateField,
+          },
+          { new: true }
+        ).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        res.json({ error: err });
       }
+      results.bug.trackActivities.push({
+        date: Date.now(),
+        updatedField: `${req.user.email} has updated the ${req.body.fieldName} to ${req.body.value}`,
+      });
+
+      results.bug.save();
+
+      // Successful, so return.
+      res.json({
+        success: results.update,
+        bug: results.bug.trackActivities,
+      });
     }
-  },
-];
+  );
+};
 
 //delete ticket
 exports.deleteTicket = (req, res) => {
