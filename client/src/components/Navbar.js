ed10366a82b42021e47f5430b8edb27e09dc004f
Undisclosed <undisclosed674@gmail.com>
2022-11-26 05:30:01 -0800

    add navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { GoSearch } from "react-icons/go";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoMdAddCircle } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav
+      id="navbar"
+      className="flex justify-between px-4 py-3 text-lightBlack3 border-b-[1px] border-borderColor"
+    >
+      <div className="sectionName">Home</div>
+      <ul className="flex">
+        <li className="mr-4 text-xl mt-0.5">
+          <Link to="#">
+            <GoSearch />
+          </Link>
+        </li>
+        <li className="mr-4 text-2xl">
+          <Link to="/notifications">
+            <IoMdNotificationsOutline />
+          </Link>
+        </li>
+        <li className="mr-4 text-2xl">
+          <Link to="/add-ticket">
+            <IoMdAddCircle className="text-brightRed " />
+          </Link>
+        </li>
+        <li className="mr-4 text-2xl">
+          <Link to="#">
+            <FaUserCircle className="text-lightGray" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
