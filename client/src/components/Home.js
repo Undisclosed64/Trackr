f74669ec200ea55b3d4c5aaf2c6fefbdb5bff0b3
Undisclosed <undisclosed674@gmail.com>
2022-12-01 08:43:40 -0800

    make info boxes responsive

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,27 +9,27 @@ const Home = ({ navbar }) => {
     <div>
       {navbar}
       <Sidebar />
-      <section id="home" className="py-10 px-3">
+      <section id="home" className="toggler py-10 px-3 md:mx-4">
         <h1 className="text-xl font-semibold">Welcome {context.userName}</h1>
         {/* container for boxes */}
-        <div className="information-boxes flex flex-col my-4">
-          <div className="box-wrapper bg-teal-500 px-2 py-6 flex flex-col items-center rounded-lg mb-4 text-white">
-            <div className="text-2xl font-bold pb-2">1</div>
+        <div className="information-boxes my-4 grid gap-8 msm:grid-cols-2 vlg:grid-cols-4">
+          <div className="box-wrapper bg-teal-500 px-2 py-8 flex flex-col items-center rounded-lg text-white">
+            <div className="text-3xl font-bold pb-2">1</div>
             <span className="text-lg">Active Projects</span>
           </div>
 
-          <div className="box-wrapper bg-red-500 px-2 py-6 flex flex-col items-center rounded-lg mb-4 text-white">
-            <div className="text-2xl font-bold pb-2">20</div>
+          <div className="box-wrapper bg-red-500 px-2 py-8 flex flex-col items-center rounded-lg text-white">
+            <div className="text-3xl font-bold pb-2">20</div>
             <span className="text-lg">Total Tickets</span>
           </div>
 
-          <div className="box-wrapper bg-cyan-500 px-2 py-6 flex flex-col items-center rounded-lg mb-4 text-white">
-            <div className="text-2xl font-bold pb-2">8</div>
+          <div className="box-wrapper bg-cyan-500 px-2 py-8 flex flex-col items-center rounded-lg text-white">
+            <div className="text-3xl font-bold pb-2">8</div>
             <span className="text-lg">Open Tickets</span>
           </div>
 
-          <div className="box-wrapper bg-teal-200 px-2 py-6 flex flex-col items-center rounded-lg mb-4 bg-yellow-500 text-white">
-            <div className="text-2xl font-bold pb-2">14</div>
+          <div className="box-wrapper bg-teal-200 px-2 py-8 flex flex-col items-center rounded-lg bg-yellow-500 text-white">
+            <div className="text-3xl font-bold pb-2">14</div>
             <span className="text-lg">Unassigned Tickets</span>
           </div>
         </div>
