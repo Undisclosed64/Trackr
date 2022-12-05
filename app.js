5d6fc2b71c2a34744eb3c2cc57c87b83e4c8224d
Undisclosed <undisclosed674@gmail.com>
2022-12-04 21:54:04 -0800

    change front-end serve

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/server", userRouter, projectRouter, bugsRouter);
 
 //serve the frontend
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.resolve(__dirname, "client", "build", "index.html"),
     function (err) {
       res.status(500).send(err);
     }
