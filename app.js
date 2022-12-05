2cf4af7bee48363ba86145dd10d4c6297ad383b5
Undisclosed <undisclosed674@gmail.com>
2022-12-04 21:28:51 -0800

    change serving front-end function

--- app.js
+++ app.js
@@ -24,13 +24,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/server", userRouter, projectRouter, bugsRouter);
 
 //serve the frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
