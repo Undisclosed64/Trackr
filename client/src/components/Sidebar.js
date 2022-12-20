d65b0cf39e2164b052118af0d1f984af74c05671
Undisclosed <undisclosed674@gmail.com>
2022-12-19 21:19:22 -0800

    change sidebar style

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -7,6 +7,8 @@ import { IoTicketOutline } from "react-icons/io5";
 import { BsListTask } from "react-icons/bs";
 import { BiBug } from "react-icons/bi";
 import { RiMenu3Line } from "react-icons/ri";
+import { IoLogoAmplify } from "react-icons/io5";
+import { FiMenu } from "react-icons/fi";
 
 const Sidebar = () => {
   const displaySidebar = () => {
@@ -38,74 +40,89 @@ const Sidebar = () => {
     sectionHome.classList.remove("toggler");
   };
   return (
-    <div className="container">
+    <div className="container ">
       {/* sidebar hamburger for smaller sizes */}
       <div
-        className="absolute top-0 left-0 px-2 py-3 w-full text-2xl border-b-[1px] border-borderColor"
+        className="hamburger-wrapper self-center px-2 py-3 absolute top-0 left-0 text-2xl"
+        id="toggleSidebar"
+        onClick={displaySidebar}
+      >
+        <FiMenu className="text-veryLightGray" />
+      </div>
+      {/* <div
+        className="absolute top-0 left-0 px-2 py-3 w-full text-2x"
         id="toggleSidebar"
         onClick={displaySidebar}
       >
         <RiMenu3Line className="text-brightOrange text-2xl" />
-      </div>
+      </div> */}
       {/* Dark themed sidebar */}
       <div
         id="sidebar"
-        className="lg:w-1/5 py-2 bg-brightBlack h-screen text-sideBarColor absolute top-0 left-0 hidden vlg:block opacity-0.9 bg-scroll"
+        className="bg-brightWhite fixed top-0 left-0 w-24 vlg:w-1/5 vlg:block h-screen"
       >
-        <div className="header px-3 mt-1 mb-3 border-b-[1px] border-sidebarBorder flex text-2xl items-center">
-          <RiMenu3Line
-            className="text-brightOrange mr-2"
+        <div className="header text-brightRed drop-shadow px-2 py-1.5 md:py-2 mb-3 text-2xl bg-brightWhite flex justify-between">
+          <div className="brand flex items-center lg:pl-1">
+            <div className="logo-wrapper w-10 h-10 rounded-full bg-brightRed mr-2 p-1">
+              <IoLogoAmplify className="text-3xl text-brightWhite" />
+            </div>
+            <div className="brandName text-4xl hidden vlg:block vlg:font-bold">
+              trackr
+            </div>
+          </div>
+          <div
+            className="hamburger-wrapper self-center mt-2 p-0.5"
             onClick={hideSidebar}
-          />
-          <BiBug className="text-brightOrange mr-2" />
-          <div className="text-brightWhite">Trackr</div>
+          >
+            <FiMenu className="text-veryLightGray" />
+          </div>
         </div>
 
-        <nav className="mx-3">
-          <ul>
-            <li className="mb-3 text-lg">
+        <nav className="mx-1 vlg:mx-3 py-3 h-screen ">
+          <ul className="font-medium mr-4 ">
+            <li className="mb-4 text-lg text-lightBlack3">
               <Link
                 to="/home"
-                className="sidebar-link flex items-center px-1 py-2 hover:bg-sidebarItemsHover hover:text-sideBarColor hover:rounded"
+                className="sidebar-link no-underline flex px-1 py-2 hover:rounded  vlg:hover:bg-lightWhite hover:text-lightBlack3 text-lightBlack3"
               >
-                <AiOutlineHome className="mr-2" />
-                <div>Home</div>
+                <AiOutlineHome className="mr-3 text-2xl" />
+                <div className="hidden text-xl vlg:block">Home</div>
               </Link>
             </li>
-            <li className="mb-3 text-lg">
+            <li className="mb-4 text-lg text-lightBlack3">
               <Link
-                to="/tickets"
-                className="flex items-center px-1 py-2 hover:bg-sidebarItemsHover hover:text-sideBarColor hover:rounded"
+                to="/feed"
+                className="flex no-underline items-center px-1 py-2 text-lightBlack3 hover:rounded vlg:hover:bg-lightWhite hover:text-lightBlack3"
               >
-                <MdOutlineFeed className="mr-2" />
-                <div>Feed</div>
+                <MdOutlineFeed className="mr-3 text-2xl" />
+                <div className="hidden text-xl vlg:block">Feed</div>
               </Link>
             </li>
-            <li className="mb-3 text-lg">
+            <li className="mb-4 text-lg text-lightBlack3">
               <Link
                 to="#"
-                className="flex items-center px-1 py-2 hover:bg-sidebarItemsHover hover:text-sideBarColor hover:rounded"
+                className="flex items-center px-1 py-2 no-underline hover:rounded vlg:hover:bg-lightWhite hover:text-lightBlack3 text-lightBlack3"
               >
-                <BsListTask className="mr-2" />
-                <div>Projects</div>
+                <BsListTask className="mr-3 text-2xl" />
+                <div className="hidden text-xl vlg:block">Projects</div>
               </Link>
             </li>
-            <li className="mb-3 text-lg">
+            <li className="mb-4 text-lg text-lightBlack3 ">
               <Link
                 to="/add-ticket"
-                className="flex items-center px-1 py-2 hover:bg-sidebarItemsHover hover:text-sideBarColor hover:rounded"
+                className="flex no-underline items-center px-1 py-2 vlg:hover:bg-lightWhite hover:text-lightBlack3 hover:rounded text-lightBlack3"
               >
-                <IoTicketOutline className="mr-2" />
-                <div>Tickets</div>
+                <IoTicketOutline className="mr-3 text-2xl" />
+                <div className="hidden text-xl vlg:block">Tickets</div>
               </Link>
             </li>
-            <li className="mb-3 text-lg">
+            <li className="mb-4 text-lg text-lightBlack3">
               <Link
                 to="/discuss"
-                className="flex items-center px-1 py-2 hover:bg-sidebarItemsHover hover:text-sideBarColor hover:rounded"
+                className="flex items-center no-underline px-1 py-2 vlg:hover:bg-lightWhite hover:text-lightBlack3 hover:rounded text-lightBlack3"
               >
-                <GoCommentDiscussion className="mr-3" />
-                <div>Discuss</div>
+                <GoCommentDiscussion className="mr-3 text-2xl" />
+                <div className="hidden text-xl vlg:block">Discuss</div>
               </Link>
             </li>
           </ul>
