3dcce7aaf6987729182117284b95416fb8716f93
Undisclosed <undisclosed674@gmail.com>
2022-12-20 10:53:05 -0800

    add search input

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -33,15 +33,19 @@ const Feed = ({ navbar }) => {
     <div>
       {navbar}
       <Sidebar />
-      <section id="feed" className="toggler py-10 px-3">
-        <div className="projects-dropdown w-full">
+      <section
+        id="feed"
+        className="toggler py-10 px-4 md:mx-20 flex justify-center flex-col"
+      >
+        <div className="projects-dropdown w-full bg-brightWhite rounded-md p-2.5 drop-shadow md:h-40 md:p-5 flex justify-center flex-col">
+          <div className="mb-3 lg:text-xl font-medium">Select Project</div>
           <select
-            className="w-full p-2.5 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600 text-center capitalize font-medium text-brightRed"
+            className=" w-full msm:w-1/2 border rounded drop-shadow-sm focus:outline-none appearance-none capitalize"
             onChange={handleChange}
           >
             {projects.map((project) => {
               return (
-                <option key={project._id} value={project._id}>
+                <option key={project._id} value={project._id} className="">
                   {project.title}
                 </option>
               );
@@ -49,6 +53,12 @@ const Feed = ({ navbar }) => {
           </select>
           ;
         </div>
+
+        <input
+          type="text"
+          className="py-3 px-2 bg-brightWhite rounded-md drop-shadow w-full my-6 border text-veryLightGray tracking-tight"
+          placeholder="Share a quick thought and start a discussion"
+        />
         {project ? <GetActivites activities={project.trackActivities} /> : ""}
       </section>
     </div>
