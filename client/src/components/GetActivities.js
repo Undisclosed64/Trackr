a8bf10ac5189ee47346f09a8fdf2f64b6fc3255b
Undisclosed <undisclosed674@gmail.com>
2022-12-21 22:07:21 -0800

    add circle on timeline

--- client/src/components/GetActivities.js
+++ client/src/components/GetActivities.js
@@ -1,15 +1,21 @@
 const GetActivites = ({ activities }) => {
   console.log(activities);
   return (
-    <div>
-      {activities.map((activity) => {
-        return (
-          <div key={activity._id}>
-            <div>{new Date(activity.date).toDateString()}</div>
-            <div>{activity.updatedField}</div>
-          </div>
-        );
-      })}
+    <div className="">
+      <div className="timeline bg-veryLightGray w-0.5 h-full shadow-lg mx-10 my-4">
+        {activities.map((activity) => {
+          return (
+            <div className="circle">
+              <div className="px-4 mb-8 w-fit lg:w-max ">
+                <div className="text-lg">{activity.updatedField}</div>
+                <div className="text-lightBlack">
+                  on {new Date(activity.date).toDateString()}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
