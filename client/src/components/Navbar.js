6baf99345cac9434aec0911c9bbed196a47bfcd9
Undisclosed <undisclosed674@gmail.com>
2023-01-01 20:42:34 -0800

    fix navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,9 +9,11 @@ const Navbar = ({ sectionName }) => {
   return (
     <nav
       id="top-navbar"
-      className="toggler flex justify-between items-center px-4 py-2 bg-brightWhite drop-shadow text-lightBlack3"
+      className="toggler flex justify-between items-center px-4 py-2 bg-brightWhite drop-shadow text-lightBlack3 fixed top-0 right-0 left-0"
     >
-      <div className="sectionName ml-4">{sectionName}</div>
+      <div className="sectionName ml-4 font-medium lg:text-lg">
+        {sectionName}
+      </div>
       <ul className="flex">
         <li className="ml-2 msm:ml-5">
           <Link to="#" className="text-lightBlack">
