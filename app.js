527c3a72cec6c6729679f73510058766b73db30a
Undisclosed <undisclosed674@gmail.com>
2023-02-05 23:55:24 -0800

    implement google login

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const projectRouter = require("./routes/project");
 const bugsRouter = require("./routes/bugs");
 const app = express();
 const cors = require("cors");
+const User = require("./models/user");
 const path = require("path");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -22,6 +27,78 @@ app.use("/server", userRouter, projectRouter, bugsRouter);
 
 //serve the frontend
 app.use(express.static("client/build"));
+
+app.use(
+  session({
+    secret: process.env.key,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+//google login strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.clientID,
+      clientSecret: process.env.clientSecret,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+    },
+
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOne({ email: profile.emails[0].value }, function (err, user) {
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+          });
+          user.save(function (err) {
+            return cb(err, user);
+          });
+        } else {
+          return cb(err, user);
+        }
+      });
+    }
+  )
+);
+
+//redirect to google login page
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+//handle the response from google after user grants/denies permission
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // console.log(req.user);
+    const accessToken = jwt.sign({ email: req.user.email }, process.env.key, {
+      expiresIn: "10h",
+    });
+
+    //return user info and token
+    // res.json({
+    //   user: req.user,
+    //   accessToken,
+    // });
+
+    // Successful authentication, redirect home.
+    res.redirect(`http://localhost:3000`);
+  }
+);
 
 app.get("*", (req, res) => {
   res.sendFile(
