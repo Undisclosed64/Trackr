54a81d34c559d5133d815e525c8ebedb83b24ebc
Undisclosed <undisclosed674@gmail.com>
2023-02-05 23:55:52 -0800

    remove required from password field

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const findOrCreate = require("mongoose-findorcreate");
 
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   role: { type: String },
 });
+
+userSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model("User", userSchema);
