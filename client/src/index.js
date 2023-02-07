eebf71c8e8e106b29f13c7f955ffb419abf498bc
Undisclosed <undisclosed674@gmail.com>
2023-02-06 23:56:39 -0800

    move router to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./errorPage";
+import Home from "./pages/Home/home";
+import Feed from "./pages/Feed/feed";
+import Ticket from "./pages/Ticket/tickets";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "feed",
+        element: <Feed />,
+      },
+      {
+        path: "tickets",
+        element: <Ticket />,
+      },
+      // {
+      //   path: "projects",
+      //   element: <Projects />,
+      // },
+      // {
+      //   path: "create-project",
+      //   element: <CreateProject />,
+      // },
+      // {
+      //   path: "projects/:projectId",
+      //   element: <ProjectDetails />,
+      // },
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+      // {
+      //   path: "tickets/:id",
+      //   element: <SingleTicket />,
+      // },
+      // {
+      //   path: "add-ticket",
+      //   element: <Ticket />,
+      // },
+      // {
+      //   path: "profile",
+      //   element: <UserProfile />,
+      // },
+      // {
+      //   path: "logout",
+      //   element: <LogOut />,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
