d38a9f82b80ddd0d6d24a9ba73ddd254507670bd
Undisclosed <undisclosed674@gmail.com>
2023-02-08 11:46:59 -0800

    seperate function for overlay

--- client/src/components/CreateFormHandler.js
+++ client/src/components/CreateFormHandler.js
@@ -3,19 +3,35 @@ import Ticket from "../components/Ticket";
 import Project from "../components/Project";
 import { useState, useEffect } from "react";
 
-export const CreateFormHandler = ({ projectCreateForm, onCancel }) => {
+export const CreateFormHandler = ({
+  projectCreateForm,
+  onCancel,
+  ticketCreateForm,
+}) => {
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showTicketForm, setShowTicketForm] = useState(false);
 
+  const addOverlayEffect = () => {
+    //select elements and add overlay effect
+    const overlay = document.querySelector(".overlay");
+    const overlayRoot = document.querySelector(".overlay-root");
+    const sidebar = document.querySelector("#sidebar");
+    const navbar = document.querySelector("#top-navbar");
+    overlay.classList.add("overlay-container");
+    overlayRoot.classList.add("overlay-container");
+    navbar.classList.add("removeZindex");
+    console.log(navbar);
+    sidebar.classList.add("hideOverflow");
+  };
   useEffect(() => {
     if (projectCreateForm) {
       setShowProjectForm(true);
-      //select elements to add overlay effect
-      const overlay = document.querySelector(".overlay");
-      const overlayRoot = document.querySelector(".overlay-root");
-      overlay.classList.add("overlay-container");
-      overlayRoot.classList.add("overlay-container");
+      addOverlayEffect();
+    } else if (ticketCreateForm) {
+      setShowTicketForm(true);
+      addOverlayEffect();
     }
-  }, [projectCreateForm]);
+  }, [projectCreateForm, ticketCreateForm]);
 
   return (
     <>
@@ -23,6 +39,11 @@ export const CreateFormHandler = ({ projectCreateForm, onCancel }) => {
 
       {showProjectForm ? (
         <Project projectCreateForm={setShowProjectForm} onCancel={onCancel} />
+      ) : (
+        ""
+      )}
+      {showTicketForm ? (
+        <Ticket ticketCreateForm={setShowTicketForm} onCancel={onCancel} />
       ) : (
         ""
       )}
