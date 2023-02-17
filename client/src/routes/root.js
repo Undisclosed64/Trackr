0c32ec6b4105101e110508c11110dbdbd18b023c
Undisclosed <undisclosed674@gmail.com>
2023-02-16 23:52:23 -0800

    change sidebar style

--- client/src/routes/root.js
+++ client/src/routes/root.js
@@ -19,20 +19,44 @@ const token = localStorage.getItem("token");
 
 const Root = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState("w-1/5");
+
   const navigate = useNavigate();
 
-  const hideSidebar = () => {
-    //select all elements
-    const sidebar = document.querySelector("#sidebar");
-    const toggleSidebar = document.querySelector("#toggleSidebar");
-
-    //perform actions on them
-    sidebar.style.display = "none";
-    toggleSidebar.style.display = "block";
+  // Get the JWT token from the cookie in google login case
+  const getTokenfromURL = () => {
+    console.log(document.cookie);
+    const cookieValue = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("accessToken="))
+      .split("=")[1];
+    // console.log(cookieValue);
+    if (cookieValue) {
+      // Save the JWT token in local storage
+      localStorage.setItem("token", cookieValue);
+    }
   };
+
+  useEffect(() => {
+    // Check if the previous page was the Google authentication page
+    // const isGoogleAuth = localStorage.getItem("isGoogleAuth") === "true";
+    // console.log(isGoogleAuth);
+    // if (isGoogleAuth) {
+    //   // Set the authentication state to true
+    //   getTokenfromURL();
+    // }
+    // getTokenfromURL();
+  }, []);
+
+  const expandMenu = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   let activeStyle = {
     color: "#FF6400",
   };
+  console.log("isLoggedIn:" + isLoggedIn);
 
   useEffect(() => {
     console.log("isLoggedIn:" + isLoggedIn);
@@ -53,24 +77,34 @@ const Root = () => {
             }
           });
       } catch (err) {
-        console.log(err);
-        navigate("/login");
+        if (err.message !== "Network Error") {
+          console.log(err);
+          navigate("/login");
+        }
       }
     };
     checkIfUserIsLoggedIn();
-  }, [isLoggedIn]);
+  }, []);
 
   return (
     <>
       <Navbar />
       <div
         id="sidebar"
-        className="bg-sidebarBg drop-shadow z-10 w-24 vlg:w-1/5 vlg:block h-screen fixed overflow-auto overflow-x-hidden sidebar"
+        className={` bg-brightblack z-10 
+        h-screen fixed overflow-auto lg:w-1/5 ${
+          isExpanded
+            ? "w-4/5 sm:w-3/5 msm:w-2/5 lg:w-1/5 transition-all duration-500 ease-out"
+            : "w-[72px] transition-all duration-500 ease-out"
+        }  `}
       >
         <div className="overlay-root hidden"></div>
 
-        <div className="header text-brightRed drop-shadow px-2 py-1.5 md:py-2 mb-3 text-2xl bg-brightWhite flex justify-between">
-          <div className="brand flex items-center lg:pl-1">
+        {/* <div
+          className="header text-brightRed 
+        border-b px-2 py-1.5 md:py-2 mb-3 text-2xl flex justify-between"
+        >
+          <div className="brand items-center lg:pl-1 hidden">
             <div className="logo-wrapper w-10 h-10 rounded-full bg-brightRed mr-2 p-1">
               <IoLogoAmplify className="text-3xl text-brightWhite" />
             </div>
@@ -78,76 +112,129 @@ const Root = () => {
               trackr
             </div>
           </div>
-          <div
-            className="hamburger-wrapper self-center mt-2 p-0.5"
-            onClick={hideSidebar}
-          >
+
+          <div className="hamburger-wrapper p-0.5" onClick={hideSidebar}>
             <FiMenu className="text-veryLightGray" />
+          </div>
+        </div> */}
+
+        <div
+          className={`header
+          border-b py-1 flex ${
+            isExpanded
+              ? "justify-evenly expanded items-center"
+              : "justify-center"
+          }`}
+        >
+          <div
+            className="hamburger-wrapper hover:bg-brightBlackHover rounded-full p-2"
+            onClick={expandMenu}
+          >
+            <FiMenu className="text-mainWhite text-2xl" />
+          </div>
+
+          <div
+            className={`brand flex items-center ${
+              isExpanded ? "expanded" : ""
+            }`}
+          >
+            <div
+              className="logo-wrapper p-1 rounded-full bg-brightRed
+             mr-1"
+            >
+              <IoLogoAmplify className="text-3xl text-brightWhite" />
+            </div>
+            <div className="brandName text-brightWhite text-2xl vlg:block vlg:font-bold">
+              trackr
+            </div>
           </div>
         </div>
 
-        <nav className="mx-1 vlg:mx-3 py-3 h-screen ">
-          <ul className="mr-4 text-lightBlack">
-            <li className="mb-4 text-lg">
+        <nav
+          className="h-screen flex justify-center
+         text-mainWhite py-8 "
+        >
+          <ul className="w-full">
+            <li className={`mb-8 w-full ${isExpanded ? "px-4" : "px-2"}`}>
               <NavLink
                 to="/home"
-                className="home-link no-underline flex px-1 py-2 hover:rounded hover:bg-lightWhite"
+                className={`lg:home-link no-underline hover:rounded hover:bg-brightBlackHover flex rounded gap-3 lg:flex-row lg:w-full lg:px-2 lg:py-3 ${
+                  isExpanded
+                    ? "flex-row w-full px-2 py-3"
+                    : "flex-col items-center px-1 py-2"
+                }`}
                 style={({ isActive }) => (isActive ? activeStyle : undefined)}
               >
-                <AiOutlineHome className="mr-3 text-2xl text-inherit" />
-                <div className="hidden text-xl vlg:block">Home</div>
+                <AiOutlineHome className="text-2xl text-inherit" />
+                <div className={`${isExpanded ? "text-xl" : "text-base"}`}>
+                  Home
+                </div>
               </NavLink>
-              {/* other code */}
             </li>
-            <li className="mb-4 text-lg">
+
+            <li className={`mb-8 w-full ${isExpanded ? "px-4" : "px-2"}`}>
               <NavLink
                 to="/feed"
-                className="flex no-underline items-center px-1 py-2  hover:rounded hover:bg-lightWhite"
+                className={`no-underline hover:rounded hover:bg-brightBlackHover flex rounded gap-3 lg:flex-row lg:w-full lg:px-2 lg:py-3  ${
+                  isExpanded
+                    ? "flex-row w-full px-2  py-3"
+                    : "flex-col items-center px-1 py-2"
+                }`}
                 style={({ isActive }) => (isActive ? activeStyle : undefined)}
               >
-                <MdOutlineFeed className="mr-3 text-2xl text-inherit" />
-                <div className="hidden text-xl vlg:block">Feed</div>
+                <MdOutlineFeed className="text-2xl text-inherit" />
+                <div className={`${isExpanded ? "text-xl" : "text-base"}`}>
+                  Feed
+                </div>
               </NavLink>
             </li>
-            <li className="mb-4 text-lg">
+            <li className={`mb-8 w-full ${isExpanded ? "px-4" : "px-2"}`}>
               <NavLink
                 to="/projects"
-                className="flex items-center px-1 py-2 no-underline hover:rounded hover:bg-lightWhite "
+                className={`no-underline hover:rounded hover:bg-brightBlackHover flex px-2 py-2 rounded gap-3 lg:flex-row lg:w-full lg:px-2 lg:py-3 ${
+                  isExpanded
+                    ? "flex-row w-full px-2 py-3"
+                    : "flex-col items-center px-1 py-2"
+                }`}
                 style={({ isActive }) => (isActive ? activeStyle : undefined)}
               >
-                <BsListTask className="mr-3 text-2xl text-inherit" />
-                <div className="hidden text-xl vlg:block">Projects</div>
+                <BsListTask className="text-2xl text-inherit" />
+                <div className={`${isExpanded ? "text-xl" : "text-base"}`}>
+                  Projects
+                </div>
               </NavLink>
             </li>
-            <li className="mb-4 text-lg">
+            <li className={`mb-8 w-full ${isExpanded ? "px-4" : "px-2"}`}>
               <NavLink
                 to="/tickets"
-                className="flex no-underline items-center px-1 py-2 vlg:hover:bg-lightWhite hover:rounded"
+                className={`no-underline hover:rounded hover:bg-brightBlackHover flex rounded gap-3 lg:flex-row lg:w-full lg:px-2 lg:py-3 ${
+                  isExpanded
+                    ? "flex-row w-full px-2 py-3"
+                    : "flex-col items-center px-1 py-2"
+                }`}
                 style={({ isActive }) => (isActive ? activeStyle : undefined)}
               >
-                <IoTicketOutline className="mr-3 text-2xl text-inherit" />
-                <div className="hidden text-xl vlg:block">Tickets</div>
+                <IoTicketOutline className="text-2xl text-inherit" />
+                <div className={`${isExpanded ? "text-xl" : "text-base"}`}>
+                  Tickets
+                </div>
               </NavLink>
             </li>
-            <li className="mb-4 text-lg">
-              <NavLink
-                to="/discuss"
-                className="flex items-center no-underline px-1 py-2 hover:bg-lightWhite 
-                hover:rounded"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-              >
-                <GoCommentDiscussion className="mr-3 text-2xl text-inherit" />
-                <div className="hidden text-xl vlg:block">Discuss</div>
-              </NavLink>
-            </li>
-            <li className="mb-4 text-lg">
+
+            <li className={`mb-8 w-full ${isExpanded ? "px-4" : "px-2"}`}>
               <NavLink
                 to="/logout"
-                className="flex items-center no-underline px-1 py-2 hover:bg-lightWhite  hover:rounded "
+                className={`no-underline hover:rounded hover:bg-brightBlackHover flex rounded gap-3 lg:flex-row lg:w-full lg:px-2 lg:py-3 ${
+                  isExpanded
+                    ? "flex-row w-full px-2 py-3"
+                    : "flex-col items-center px-1 py-2 "
+                }`}
                 style={({ isActive }) => (isActive ? activeStyle : undefined)}
               >
-                <FiLogOut className="mr-3 text-2xl text-inherit" />
-                <div className="hidden text-xl vlg:block">Logout</div>
+                <FiLogOut className="text-2xl text-inherit" />
+                <div className={`${isExpanded ? "text-xl" : "text-base"} `}>
+                  Logout
+                </div>
               </NavLink>
             </li>
           </ul>
