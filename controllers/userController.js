7e6e4b1aca10515f3fe0fd259cd52a039e21b34d
Undisclosed <undisclosed674@gmail.com>
2023-02-21 00:34:50 -0800

    add demo user option in login

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,68 +4,91 @@ const { validationResult, check } = require("express-validator");
 const jwt = require("jsonwebtoken");
 
 exports.createUser = [
-  //validate input fields
+  // Validate input fields
   check("firstName").not().isEmpty().withMessage("First name can not be empty"),
   check("email").isEmail().withMessage("Please enter a valid email address"),
   check("password")
     .isLength({ min: 4 })
     .withMessage("Password must be at least 4 characters long"),
 
+  // Check for duplicate email
+  check("email").custom(async (email) => {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new Error("Email already exists");
+    }
+  }),
+
   async (req, res) => {
-    //check for errors in req data
+    // Check for validation errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
-    } else {
-      //create user object
-      const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-        role: req.body.role,
-      });
-
-      //secure the password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-
-      //save the user
-      user.save((err, created_user) => {
-        if (err) res.status(500).json(err);
-        res.json(created_user);
-      });
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    // Create user object
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role,
+    });
+
+    // Secure the password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    // Save the user
+    user.save((err, created_user) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.json(created_user);
+    });
   },
 ];
 
 exports.logUser = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-
-  //check if email is registered or not
-  if (user === null) {
-    res.status(404).json({ msg: "Email is not registered!" });
-    return;
-  }
-  console.log("user" + user);
-  //check if password is correct or not
-  if (await bcrypt.compare(req.body.password, user.password)) {
-    //generate an access token
+  if (req.query.demoUser) {
+    const user = await User.find({ email: "demouser@gmail.com" });
     const accessToken = jwt.sign(
-      { email: user.email, role: user.role },
+      { email: "demouser@gmail.com", role: "developer" },
       process.env.key,
       { expiresIn: "10h" }
     );
+    // console.log(user);
     //return user info and token
     res.json({
       user,
       accessToken,
     });
   } else {
-    res.status(401).json({ msg: "Password is incorrect!" });
+    const user = await User.findOne({ email: req.body.email });
+
+    //check if email is registered or not
+    if (user === null) {
+      res.status(404).json({ msg: "Email is not registered!" });
+      return;
+    }
+    //check if password is correct or not
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      //generate an access token
+      const accessToken = jwt.sign(
+        { email: user.email, role: user.role },
+        process.env.key,
+        { expiresIn: "10h" }
+      );
+      //return user info and token
+      res.json({
+        user,
+        accessToken,
+      });
+    } else {
+      res.status(401).json({ msg: "Password is incorrect!" });
+    }
   }
 };
-
 // exports.deleteAll = function (req, res) {
 //   User.remove({}, function(err,success){
 //     if(err)res.json(err);
