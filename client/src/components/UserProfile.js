4a138b3c3d649965d33d5516752425d3d18a8d36
Undisclosed <undisclosed674@gmail.com>
2023-02-20 19:17:00 -0800

    change user profile style

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -9,17 +9,25 @@ const Profile = () => {
   console.log(user);
   if (!user) return <div>Loading..</div>;
   return (
-    <div className="bg-profileBg h-96 pt-20 text-lightBlack3 flex flex-col justify-center items-center md:flex-row gap-2">
-      <div className="img-wrapper bg-orange-100 p-2 items-center flex justify-center w-36 h-36 rounded-full border-[2px]">
-        <FiUser className="text-5xl text-lightGray" />
-      </div>
-      <div className="info md:flex md:flex-col">
-        <div className="name text-3xl font-medium">
-          {user.firstName} {user.lastName}
+    <section className="bg-profileBg h-96 pt-20 text-lightBlack3">
+      <div className="header pt-10 flex flex-col justify-center items-center md:flex-row gap-2 pb-4">
+        <div className="img-wrapper bg-orange-100 p-2 items-center flex justify-center w-32 h-32 rounded-full border-[2px]">
+          <FiUser className="text-4xl text-lightGray" />
         </div>
-        <div className="text-xl text-lightGray pt-2">{user.email}</div>
+        <div className="info md:flex md:flex-col md:pl-4">
+          <div className="name text-3xl font-bold capitalize">
+            {user.firstName} {user.lastName}
+          </div>
+          <div className="text-lg font-medium text-brightOrange pt-2 capitalize">
+            {user.role}
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="mt-8 flex text-lg bg-brightWhite drop-shadow md:w-1/2 px-2 py-4 h-1/2 mx-4 rounded justify-center md:mx-auto">
+        <span className="text-lightGray font-medium mr-4">E-mail: </span>
+        <div className="">{user.email}</div>
+      </div>
+    </section>
   );
 };
 
