e4ebaa81d479ef2758ce36275d39e229789617ae
Undisclosed <undisclosed674@gmail.com>
2023-02-20 19:48:05 -0800

    remove findorcreate

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const findOrCreate = require("mongoose-findorcreate");
 
 const userSchema = new Schema({
   firstName: { type: String, required: true },
@@ -9,7 +8,5 @@ const userSchema = new Schema({
   password: { type: String },
   role: { type: String },
 });
-
-userSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model("User", userSchema);
