a269f5b159dbfab5f044c722918aced194eba500
Undisclosed <undisclosed674@gmail.com>
2023-02-20 23:39:26 -0800

    access name prop

--- client/src/components/Empty.js
+++ client/src/components/Empty.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Project from "./Project";
 import { CreateFormHandler } from "./CreateFormHandler";
 
-const Empty = () => {
+const Empty = ({ name }) => {
   const [createProject, setCreateProject] = useState(false);
   const handleProjectCreate = () => {
     setCreateProject(true);
@@ -33,13 +33,13 @@ const Empty = () => {
       )}
       <img src={emptyList} alt="emptyList" className="md:w-1/4" />
       <div className="text-lg font-medium text-center text-lightGray">
-        Looks like you don't have any project yet.
+        Looks like you don't have any {name} yet.
       </div>
       <p
         className="text-center underline hover:cursor-pointer hover:text-brightOrange text-lightGray "
         onClick={handleProjectCreate}
       >
-        Add a project to track it
+        Add a {name} to track it
       </p>
     </section>
   );
