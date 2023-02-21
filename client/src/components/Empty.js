c5d4725f1d670a36eb4757b354ee6b6d6248d4fe
Undisclosed <undisclosed674@gmail.com>
2023-02-20 23:25:13 -0800

    add project create link in empty

--- client/src/components/Empty.js
+++ client/src/components/Empty.js
@@ -1,13 +1,46 @@
 import emptyList from "../assets/emptyList.png";
+import { useState } from "react";
+import Project from "./Project";
+import { CreateFormHandler } from "./CreateFormHandler";
 
 const Empty = () => {
+  const [createProject, setCreateProject] = useState(false);
+  const handleProjectCreate = () => {
+    setCreateProject(true);
+  };
+  const handleCancel = () => {
+    setCreateProject(false);
+    //remove overflow effects
+    const overlayRoot = document.querySelector(".overlay-root");
+    overlayRoot.classList.remove("overlay-container");
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.classList.remove("hideOverflow");
+    const navbar = document.querySelector("#top-navbar");
+    navbar.classList.remove("removeZindex");
+  };
   return (
-    <section className="empty-wrapper flex justify-center items-center h-screen flex-col px-4 text-lightGray gap-2">
+    <section
+      className="empty-wrapper flex justify-center items-center h-screen flex-col px-4 
+    gap-2"
+    >
+      {createProject ? (
+        <CreateFormHandler
+          projectCreateForm={createProject}
+          onCancel={handleCancel}
+        />
+      ) : (
+        ""
+      )}
       <img src={emptyList} alt="emptyList" className="md:w-1/4" />
-      <div className="text-lg font-medium text-center">
+      <div className="text-lg font-medium text-center text-lightGray">
         Looks like you don't have any project yet.
       </div>
-      <p className="text-center">Add a project to track it.</p>
+      <p
+        className="text-center underline hover:cursor-pointer hover:text-brightOrange text-lightGray "
+        onClick={handleProjectCreate}
+      >
+        Add a project to track it
+      </p>
     </section>
   );
 };
