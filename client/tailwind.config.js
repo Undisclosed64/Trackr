2af3b8c4660ae061ddf89cafa41a0ccfdaefb2ca
Undisclosed <undisclosed674@gmail.com>
2023-03-11 05:50:59 -0800

    add .jsx extension to content

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.js"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     screens: {
       vsm: "200px",
