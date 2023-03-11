8f2536175a1172b951ef01b7f9a552531ccb689f
Undisclosed <undisclosed674@gmail.com>
2023-03-11 05:51:20 -0800

    add vite config

---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+const svgrPlugin = require("vite-plugin-svgr");
+
+module.exports = defineConfig({
+  plugins: [react(), svgrPlugin()],
+  css: {
+    preprocessorOptions: {
+      postcss: require("./postcss.config.js"),
+    },
+  },
+});
